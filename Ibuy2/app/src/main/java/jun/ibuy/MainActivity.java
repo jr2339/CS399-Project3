93cef08788e8a3001a6117c66f10e0ee4b3f514a
jr2339 <jr2339@nau.edu>
2016-11-07 21:16:15 -0700

    Add features

--- Ibuy2/app/src/main/java/jun/ibuy/MainActivity.java
+++ Ibuy2/app/src/main/java/jun/ibuy/MainActivity.java
@@ -1,75 +1,102 @@
 package jun.ibuy;
 
-import android.icu.util.RangeValueIterator;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import java.text.*;
-import java.util.*;
-import java.lang.String;
+import android.widget.Toast;
 
+public class MainActivity extends AppCompatActivity {
+    private EditText itemName, itemCost;
+    private Button submitButton;
+    private DatabaseHandler dba;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private ArrayList<String> shoppingList = new ArrayList<String>();
-
-    private EditText itemName,itemDate,itemCost,itemUrgency,itemListView;
-    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setTitle("Shopping List");
+        setContentView(R.layout.activity_main);
 
-         itemName = (EditText)findViewById(R.id.item_name);
-         itemDate = (EditText)findViewById(R.id.item_date);
-         itemCost = (EditText)findViewById(R.id.item_cost);
-         itemUrgency = (EditText)findViewById(R.id.item_urgency);
 
-        ListView shoppingListView = (ListView)findViewById(R.id.item_list);
+        dba = new DatabaseHandler(MainActivity.this);
 
-        Button addItem = (Button) findViewById(R.id.add_item);
-        addItem.setOnClickListener(this);
+        itemName = (EditText) findViewById(R.id.itemEditText);
+        itemCost = (EditText) findViewById(R.id.costEditText);
+        submitButton = (Button) findViewById(R.id.submitButton);
 
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,shoppingList);
-        shoppingListView.setAdapter(adapter);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                saveDataToDB();
+
+            }
+        });
+    }
+
+    private void saveDataToDB() {
+
+        Item item = new Item();
+        String name = itemName.getText().toString().trim();
+        String cost = itemCost.getText().toString().trim();
+
+        int cals = Integer.parseInt(cost);
+
+        if (name.equals("") || cost.equals("")) {
+
+            Toast.makeText(getApplicationContext(), "No empty fields allowed", Toast.LENGTH_LONG).show();
+
+        }else {
+
+            item.setItemName(name);
+            item.setCost(cals);
+
+
+            dba.addItem(item);
+            dba.close();
+
+
+            //clear the form
+            itemName.setText("");
+            itemCost.setText("");
+
+            //take users to next screen (display all entered items)
+            startActivity(new Intent(MainActivity.this, DisplayItemActivity.class));
+        }
 
     }
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home){
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
-    }
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-    @Override
-    public void onClick(View v) {
-        if (!itemName.getText().toString().equals("")){
-            shoppingList.add(itemName.getText().toString()+"     "+itemDate.getText().toString()+"       "+itemCost.getText().toString()+"       "+itemUrgency.getText().toString());
-            adapter.notifyDataSetChanged();
-            itemName.setText("");
-            itemCost.setText("");
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 }
